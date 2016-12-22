9694bd2a1c97e434d341c7583a250f8e2bda8464
Ivo Vos <vos.ivo@gmail.com>
2016-12-22 23:01:29 +0000

    Updates JSON Parse
    included support for types with whitespaes

--- js/client.js
+++ js/client.js
@@ -10,28 +10,12 @@ $(document).ready(function() {
         });
     });
 
-
-    //  utility methods for working with arrays
-    Array.prototype.contains = function(v) {
-        for (var i = 0; i < this.length; i++) {
-            if (this[i] === v) return true;
-        }
-        return false;
-    };
-
-    Array.prototype.unique = function() {
-        var arr = [];
-        for (var i = 0; i < this.length; i++) {
-            if (!arr.contains(this[i])) {
-                arr.push(this[i]);
-            }
-        }
-        return arr;
-    }
-
     // create a new array with all the unique types
     var types = new Array();
-    var uniqueTypes = types.unique();
+
+    var variable = "This is it...";
+    var variableNoWhiteSpace = variable.replace(/\s+/g, '');
+    console.log(variableNoWhiteSpace);
 
     // Parses the JSON into a  list organised by type of product
     $.getJSON('js/orders.json', function(data) {
@@ -41,16 +25,16 @@ $(document).ready(function() {
         });
 
         // figure out the unique ones
-        uniqueTypes = types.unique();
+        uniqueTypes = _.uniq(types);
         // create a list and header for each type
         $(uniqueTypes).each(function(index, value) {
-            $('<ul id=' + uniqueTypes[index].toLowerCase() + 'List><h3></h3></ul>').appendTo('#orderList');
-            $('#' + uniqueTypes[index].toLowerCase() + 'List h3').text(uniqueTypes[index]);
+            $('<ul id=' + uniqueTypes[index].toLowerCase().replace(/\s+/g, '') + 'List><h3></h3></ul>').appendTo('#orderList');
+            $('#' + uniqueTypes[index].toLowerCase().replace(/\s+/g, '')  + 'List h3').text(uniqueTypes[index]);
         });
         // add the items to the right list
         $(data.items).each(function(index, value) {
             if (value.available == true) {
-                $('<li></li>').text(value.name).appendTo('#' + value.type.toLowerCase() + 'List');
+                $('<li></li>').text(value.name).appendTo('#' + value.type.toLowerCase().replace(/\s+/g, '')  + 'List');
             }
         });
     });
